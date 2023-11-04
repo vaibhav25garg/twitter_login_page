284dad0fab80047f9926ec52bd405ccbfa06d15a
Vaibhav <vaibhavgarg977@gmail.com>
2023-11-05 01:32:15 +0530

    first twitt commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm:'500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1400px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
